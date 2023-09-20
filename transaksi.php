c42812e05e97ffbbdf87003ef380ee19761b3542
ROsidin <144438823+RosidinFahmi@users.noreply.github.com>
2023-09-20 20:49:54 +0700

    Add files via upload
    Transaksi uang

--- transaksi.php
+++ transaksi.php
@@ -1,44 +1,31 @@
 <?php
-// Data transaksi terakhir (contoh)
-$transactions = [
-        1 => ['amount' => -500, 'description' => 'Pengeluaran', 'date' => '2023-09-12'],
-        2 => ['amount' => 1000, 'description' => 'Pemasukan', 'date' => '2023-09-11'],
-    // Tambahkan data transaksi lainnya sesuai kebutuhan
-];
+if(isset($_POST)) {
+    $key = isset($_POST['key']) ? $_POST[ 'key']: '';
+    //ambil data
+    $pengirim = $_POST["pengirim"];
+    $penerima = $_POST["penerima"];
+    $jumlah = $_POST["jumlah"];
+    
+    //validasi data
+    $validasi_sukses = true; //implementasi validasi sesuai kebutuhan
 
+    if ($validasi_sukses){
 
-// Mendapatkan daftar transaksi terakhir untuk akun tertentu
-function getTransactions($accountId) {
-        global $transactions;
-        $accountTransactions = [];
-        foreach ($transactions as $id => $transaction) {
-        if ($accountId == $id) {
-                $accountTransactions[] = $transaction;
-        }
-        }
-        return $accountTransactions;
-}
-
-// Memeriksa metode HTTP
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        // Memeriksa apakah parameter 'account_id' ada dalam URL
-        if (isset($_GET['account_id'])) {
-                $accountId = $_GET['account_id'];
-                $accountTransactions = getTransactions($accountId);
-
-        if (!empty($accountTransactions)) {
-                echo json_encode(['transactions' => $accountTransactions]);
+        $transfer_sukses = true; //implementasi data transfer
+        if ($transfer_sukses){
+            echo "Transfer uang berhasil";
         } else {
-                http_response_code(404);
-                echo json_encode(['error' => 'Akun tidak memiliki transaksi']);
+            echo "Transfer uang gagal, silakan coba lagi";
         }
-        } else {
-                http_response_code(400);
-                echo json_encode(['error' => 'ID akun harus disertakan']);
-        }
-} else {
-        http_response_code(405);
-        echo json_encode(['error' => 'Metode HTTP tidak diizinkan']);
+    } else {
+        echo "Data pengirim atau penerima tidak valid. Silakan periksa kembali";
+    }
 }
-
+    function rupiah($angka){
+        //penambahan mata uang untuk proses tranfers berhasil
+        $hasil_rupiah = " Rp " . number_format($angka,2,',','.');
+        return $hasil_rupiah;
+    }
+    echo rupiah(250000);
 ?>
+
